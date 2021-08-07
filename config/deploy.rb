bc07ab6da21e2e85b6f53e2a55061ec2c4869b4d
Christian <ctingle18@gmail.com>
2021-08-07 19:30:05 -0400

    lkwejde

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,4 +40,4 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-#set :linked_files, "/home/deploy/master.key"
+set :linked_files, %w{config/credentials/production.key}
