ecf2b0b29494dccb788d03df3c55265d03b46c02
Christian <ctingle18@gmail.com>
2021-08-07 20:12:27 -0400

    Finally got production working

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   enum access_level: {admin: 100, user: 50, no_access: 0}
 
+  validates :name, :email, :password, presence: true
+
   def admin_access?
     return self.admin?
   end
